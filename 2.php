f441dc971183dbca41cc87fc6ac6ac77f562b4c4
Nadin Nur Yunia Syafitri <142859063+fitrinadin@users.noreply.github.com>
2023-08-23 08:16:17 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+  <?php
+  $color = "merah";
+  echo "Rumah Saya" . $color . "<br>";
+  echo "Motor Saya" . $color . "<br>";
+  echo "Jaket Saya" . $color . "<br>";
+  ?>
+
+</body>
+</html>
